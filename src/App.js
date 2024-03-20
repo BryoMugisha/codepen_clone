6aa922c7b19c0f5188f667ce0a6c0ef3547ff1ea
VLE Support <vle.support@isbatuniversity.com>
2024-03-20 15:45:12 +0300

    add project files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import Editor from './Editor';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Editor />
+    </div>
+  )
+}
+
+export default App;
